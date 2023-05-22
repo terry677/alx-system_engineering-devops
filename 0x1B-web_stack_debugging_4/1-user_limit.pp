3842a342c199324277bbdc92f6bffbf0f9ecb150
terry677 <terryabu67@gmail.com>
2023-05-22 03:21:06 +0000

    task added

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# this script anable the user 'holberton' to login and open files without getting error.
+
+# Increase hard file limit for the user holberton.
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/',
+}
+
+# Increase soft file limit for the user holberton.
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/',
+}
